7d1571078e0c3d7167c38ea413f7fbd74697ba5e
Merts Savelii <3ave115mr.s@gmail.com>
2022-10-10 22:41:30 +0300

    cf

--- Discrete_Analysis/lab_5/lab5.cpp
+++ Discrete_Analysis/lab_5/lab5.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <algorithm>
-#include "suff_tree.hpp"
+#include "suff_tree_linear.hpp"
 
 int main() {
     std::string in_str;
     std::cin >> in_str;
-    SuffTree tree;
-    tree.Insert(in_str);
+    SuffixTree tree(in_str);
+    //tree.Insert(in_str);
     std::string in_substr;
     int i = 1;
     while (std::cin >> in_substr) {
-        std::vector<unsigned> ans;
-        tree.Find(in_substr, ans);
+        std::vector<int> ans;
+        tree.FindSubStr(in_substr, ans);
         std::sort(ans.begin(), ans.end());
         if(!ans.empty())
         {
