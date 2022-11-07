a674a7aa53f79c5ad3feb368685f713d70064731
Merts Savelii <3ave115mr.s@gmail.com>
2022-11-07 10:56:34 +0300

    Dijkstras_algorithm complite

--- Discrete_Analysis/lab_9/lab9.cpp
+++ Discrete_Analysis/lab_9/lab9.cpp
@@ -1,47 +1,43 @@
-#include <iostream>
-#include <queue>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 const long long INF = INT64_MAX;
-long long V_size;
 
-vector<long long> Dijkstras_algorithm(vector<vector<long long>> adjacency_matrix, long long start_V){
-    vector<bool> visited(V_size, false);
-    vector<long long> distance(V_size, INF);
+vector<long long> Dijkstras_algorithm(vector<unordered_map<unsigned, long long>> adjacency_vec, long long start_V){
+    vector<bool> visited(adjacency_vec.size(), false);
+    vector<long long> distance(adjacency_vec.size(), INF);
+    priority_queue<pair<long long, unsigned>, vector<pair<long long, unsigned>>, greater<>> v_to_visit;
+
     distance[start_V] = 0;
-    long long min, min_idx;
-    do{
-        min = INF, min_idx = INF;
-        for(long long i = 0; i < V_size; ++i)
-            if(!visited[i] && distance[i] < min)
-                min = distance[i], min_idx = i;
-
-        if(min_idx != INF){
-            for(long long i = 0; i < V_size; ++i)
-                if(adjacency_matrix[min_idx][i] > 0)
-                    if(min + adjacency_matrix[min_idx][i] < distance[i])
-                        distance[i] = min + adjacency_matrix[min_idx][i];
-            visited[min_idx] = true;
+    v_to_visit.push({0, start_V});
+    while(!v_to_visit.empty()){
+        long long v = v_to_visit.top().second;
+        v_to_visit.pop();
+        if(!visited[v]){
+            for(auto& j: adjacency_vec[v]){
+                if(distance[v] + j.second < distance[j.first]){
+                    distance[j.first] = distance[v] + j.second;
+                    v_to_visit.push({distance[j.first], j.first});
+                }
+            }
+            visited[v] = true;
         }
-    } while (min_idx < INF);
+    }
     return distance;
 }
 
 int main(){
-    long long E_size;
+    long long V_size, E_size;
     long long start_V, finish_V;
     cin >> V_size >> E_size >> start_V >> finish_V;
-    vector<vector<long long>> adjacency_matrix (V_size, {V_size, 0});
+    vector<unordered_map<unsigned, long long>> adjacency_vec (V_size);
     for(long long i = 0; i < E_size; ++i){
-        long long v1, v2;
-        cin >> v1 >> v2 >> adjacency_matrix[v1][v2];
+        long long v1, v2, length;
+        cin >> v1 >> v2 >> length;
+        adjacency_vec[v1 - 1].insert({v2 - 1, length});
+        adjacency_vec[v2 - 1].insert({v1 - 1, length});
     }
-    vector<long long> min_distance = Dijkstras_algorithm(adjacency_matrix, start_V);
-    for(int i = 0; i < V_size; ++i){
-        cout << min_distance[i] << ' ';
-    }
-    cout<<endl;
+    
     return 0;
 }
