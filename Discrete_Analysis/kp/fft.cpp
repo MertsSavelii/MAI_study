179681f91fe5500a9abe84f15a9aa75610c29ff6
Merts Savelii <3ave115mr.s@gmail.com>
2022-12-05 23:05:35 +0300

    bd kp

--- Discrete_Analysis/kp/fft.cpp
+++ Discrete_Analysis/kp/fft.cpp
@@ -1,6 +1,7 @@
 #include <complex>
 #include <vector>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
@@ -32,10 +33,11 @@ void fft (vector<base> & a, bool invert) {
 	}
 }
 
-void multiply (const vector<int> & a, const vector<int> & b, vector<int> & res) {
-	vector<base> fa (a.begin(), a.end()),  fb (b.begin(), b.end());
+void MultiplyBigNum (const vector<int> & poli1, const vector<int> & poli2, vector<int> & res) {
+	vector<base> fa (poli1.begin(), poli1.end()),  fb (poli2.begin(), poli2.end());
 	size_t n = 1;
-	while (n < max (a.size(), b.size()))  n <<= 1;
+	while (n < max (poli1.size(), poli2.size()))
+		n <<= 1;
 	n <<= 1;
 	fa.resize (n),  fb.resize (n);
  
@@ -47,4 +49,35 @@ void multiply (const vector<int> & a, const vector<int> & b, vector<int> & res) 
 	res.resize (n);
 	for (size_t i=0; i<n; ++i)
 		res[i] = int (fa[i].real() + 0.5);
+	int carry = 0;
+	//нормализуем результат
+	for (size_t i=0; i<n; ++i) {
+		res[i] += carry;
+		carry = res[i] / 10;
+		res[i] %= 10;
+	}
+}
+
+void BigNumToPolinom(string big_num, vector<int>& poli){
+	for(int i = 0; i < big_num.length(); ++i){
+		poli.push_back(big_num[i] - '0');
+	}
+}
+
+void PolinomToBigNum(vector<int> poli, string& big_num){
+	for(int i = 0; i < poli.size(); ++i){
+		big_num.push_back(poli[i] + '0');
+	}
+}
+
+int main(){
+	string big_num1, big_num2, res_big_num;
+	vector<int> poli1, poli2, res_poli;
+	cin >> big_num1 >> big_num2;
+	BigNumToPolinom(big_num1, poli1);
+	BigNumToPolinom(big_num2, poli2);
+	MultiplyBigNum(poli1, poli2, res_poli);
+	PolinomToBigNum(res_poli, res_big_num);
+	cout << res_big_num << endl;
+	return 0;
 }
