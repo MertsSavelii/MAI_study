0529fdb860ee20e3321900ee795ac88a3319143e
Merts Savelii <3ave115mr.s@gmail.com>
2022-12-06 23:36:21 +0300

    з

--- Discrete_Analysis/kp/fft.cpp
+++ Discrete_Analysis/kp/fft.cpp
@@ -1,83 +1,97 @@
-#include <complex>
-#include <vector>
-#include <cmath>
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef complex<double> base;
- 
-void fft (vector<base> & a, bool invert) {
-	int n = (int) a.size();
+typedef complex<double> cd;
+
+vector<cd> fft(vector<cd>& a)
+{
+	int n = a.size();
 	if (n == 1)
-        return;
- 
-	vector<base> a0 (n/2),  a1 (n/2);
-	for (int i=0, j=0; i<n; i+=2, ++j) {
-		a0[j] = a[i];
-		a1[j] = a[i+1];
-	}
-	fft (a0, invert);
-	fft (a1, invert);
- 
-	double ang = 2*M_PI /n;
-    ang *= invert ? -1 : 1;
+		return vector<cd>(1, a[0]);
 
-	base w (1),  wn (cos(ang), sin(ang));
-	for (int i=0; i<n/2; ++i) {
-		a[i] = a0[i] + w * a1[i];
-		a[i+n/2] = a0[i] - w * a1[i];
-		if (invert)
-			a[i] /= 2,  a[i+n/2] /= 2;
-		w *= wn;
+	vector<cd> w(n);
+	for (int i = 0; i < n; i++) {
+		double alpha = -2 * M_PI * i / n;
+		w[i] = cd(cos(alpha), sin(alpha));
 	}
+
+	vector<cd> A0(n / 2), A1(n / 2);
+	for (int i = 0; i < n / 2; i++) {
+		A0[i] = a[i * 2];
+		A1[i] = a[i * 2 + 1];
+	}
+
+	vector<cd> y0 = fft(A0);
+	vector<cd> y1 = fft(A1);
+
+	vector<cd> y(n);
+
+	for (int k = 0; k < n / 2; k++) {
+		y[k] = y0[k] + w[k] * y1[k];
+		y[k + n / 2] = y0[k] - w[k] * y1[k];
+	}
+	return y;
 }
 
-void MultiplyBigNum (const vector<int> & poli1, const vector<int> & poli2, vector<int> & res) {
-	vector<base> fa (poli1.begin(), poli1.end()),  fb (poli2.begin(), poli2.end());
+vector<cd> ifft(vector<cd>& y)
+{
+	y = fft(y);
+	for(int i = 0; i < y.size(); ++i){
+		y[i] /= y.size();
+	}
+	vector<cd> a;
+	a.push_back(y[0].real());
+	for(int i = y.size() - 1; i >= 1; --i){
+		a.push_back(y[i].real());
+	}
+	return a;
+}
+
+vector<cd> multiply(vector<cd> a, vector<cd> b){
 	size_t n = 1;
-	while (n < max (poli1.size(), poli2.size()))
+	while(n < max(a.size(), b.size()))
 		n <<= 1;
-	n <<= 1;
-	fa.resize (n),  fb.resize (n);
- 
-	fft (fa, false),  fft (fb, false);
-	for (size_t i=0; i<n; ++i)
-		fa[i] *= fb[i];
-	fft (fa, true);
- 
-	res.resize (n);
-	for (size_t i=0; i<n; ++i)
-		res[i] = int (fa[i].real() + 0.5);
-	int carry = 0;
-	//нормализуем результат
-	for (size_t i=0; i<n; ++i) {
-		res[i] += carry;
-		carry = res[i] / 10;
-		res[i] %= 10;
+	a.resize(n), b.resize(n);
+	vector<cd> ya = fft(a);
+	vector<cd> yb = fft(b);
+	vector<cd> res(n);
+	for(int i = 0; i < n; i++){
+		res[i] = ya[i] * yb[i];
 	}
+	res = ifft(res);
+	// нужно убрать незначащие нули
+	// потом сделать пернос разрядов и всё будет тип топ
+	return res;
 }
 
-void BigNumToPolinom(string big_num, vector<int>& poli){
-	for(int i = 0; i < big_num.length(); ++i){
-		poli.push_back(big_num[i] - '0');
+vector<cd> string_to_polinom(string& str){
+	vector<cd> res;
+	for(int i = str.length() - 1; i >= 0; i--){
+		if(isdigit(str[i]))
+			res.push_back(str[i] - '0');
 	}
+	return res;
 }
 
-void PolinomToBigNum(vector<int> poli, string& big_num){
-	for(int i = 0; i < poli.size(); ++i){
-		big_num.push_back(poli[i] + '0');
+string polinom_to_string(vector<cd> poli){
+	string res;
+	for(int i = poli.size() - 1; i >= 0; i--){
+		res.push_back(poli[i].real() + '0');
 	}
+	return res;
 }
 
-int main(){
-	string big_num1, big_num2, res_big_num;
-	vector<int> poli1, poli2, res_poli;
-	cin >> big_num1 >> big_num2;
-	BigNumToPolinom(big_num1, poli1);
-	BigNumToPolinom(big_num2, poli2);
-	MultiplyBigNum(poli1, poli2, res_poli);
-	PolinomToBigNum(res_poli, res_big_num);
-	cout << res_big_num << endl;
+int main()
+{
+	string in_big_num;
+	cin >> in_big_num;
+	vector<cd> a = string_to_polinom(in_big_num);
+	cin >> in_big_num;
+	vector<cd> b = string_to_polinom(in_big_num);
+	vector<cd> res = multiply(a, b);
+	
+	for(int i = 0; i < a.size() + b.size() - 1; i++){
+		cout << res[i] << endl;
+	}
 	return 0;
 }
