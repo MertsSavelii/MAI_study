91337fde401c6f0d5cc1ef4c9fbd46b78d7c7b20
Savelii Merz <3ave115mr.s@gmail.com>
2023-03-17 13:10:41 +0300

    lab1-2 done

--- Numerical_Methods/lab1/lab1-2.hpp
+++ Numerical_Methods/lab1/lab1-2.hpp
@@ -1,48 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void Read_Tridiagonal_Matrix (vector <double> &a, 
-                              vector <double> &b, 
-                              vector <double> &c,
-                              vector <double> &d, 
-                              vector <double> &P, 
-                              vector <double> &Q,
-                              int n){
-    cin >> b[0] >> c[0] >> d[0];
-    P[0] = -c[0]/b[0];
-    Q[0] = d[0]/b[0];
+void Read_Tridiagonal_Matrix_From_Normal_Matrix 
+                           (vector<vector <double>> A,
+                            vector <double> B,
+                            vector <double> &a, 
+                            vector <double> &b, 
+                            vector <double> &c,
+                            vector <double> &d,
+                            int n){
+    d = B;
+    a[0] = 0, b[0] = A[0][0], c[0] = A[0][1];
     for(int i = 1; i < n-1; i++){
-        cin >> a[i] >> b[i] >> c[i] >> d[i];
-        P[i] = -c[i] / (b[i] + a[i]*P[i-1]);
-        Q[i] = (d[i] - a[i]*Q[i-1]) / (b[i] + a[i]*P[i-1]);
+        a[i] = A[i][i-1];
+        b[i] = A[i][i];
+        c[i] = A[i][i+1];
     }
-    cin >> a[n-1] >> b[n-1]>> b[n-1];
-    Q[n-1] = (d[n-1] - a[n-1]*Q[n-2]) / (b[n-1] + a[n-1]*P[n-2]);
+    a[n-1] = A[n-1][n-2], b[n-1] = A[n-1][n-1], c[n-1] = 0;
 }
 
-void Direct_Passage (vector <double> &a, 
-                     vector <double> &b, 
-                     vector <double> &c,
-                     vector <double> &d, 
+void Read_Tridiagonal_Matrix(vector <double> &a, 
+                             vector <double> &b, 
+                             vector <double> &c,
+                             vector <double> &d,
+                             int n){
+    cin >> b[0] >> c[0] >> d[0];
+    for(int i = 1; i < n-1; i++)
+        cin >> a[i] >> b[i] >> c[i] >> d[i];
+    cin >> a[n-1] >> b[n-1] >> d[n-1];
+}
+
+void Direct_Passage (vector <double> a, 
+                     vector <double> b, 
+                     vector <double> c,
+                     vector <double> d, 
                      vector <double> &P, 
                      vector <double> &Q,
                      int n){
-    for(int i = 1; i < n-1; i++){
+    for(int i = 0; i < n; i++){
         P[i] = -c[i] / (b[i] + a[i]*P[i-1]);
         Q[i] = (d[i] - a[i]*Q[i-1]) / (b[i] + a[i]*P[i-1]);
     }
 }
 
-int main(){
-    int n; cin >> n;
-    vector <double> a(n, 0), b(n, 0), c(n, 0),
-                    d(n, 0), P(n, 0), Q(n, 0);
-    Read_Tridiagonal_Matrix(a, b, c, d, P, Q, n);
-    vector <double> x(n, 0);
+void Reverse_Passage (vector <double> &x,
+                      vector <double> P,
+                      vector <double> Q,
+                      int n){
     x[n-1]= Q[n-1];
     for(int i = n-2; i >= 0; i--)
         x[i] = P[i]*x[i+1] + Q[i];
-    for(int i = 0; i < n; i++)
-        cout << x[n] << endl;
-    return 0;
+}
+
+void Solve_With_Run_Through(vector <vector <double>> &A,
+			       vector <double> &B,
+                   vector <double> &x,
+                  int n){
+    vector <double> a(n, 0), b(n, 0), c(n, 0),
+                    d(n, 0), P(n, 0), Q(n, 0);
+    Read_Tridiagonal_Matrix_From_Normal_Matrix(A, B, a, b, c, d, n);
+    Direct_Passage(a, b, c, d, P, Q, n);
+    Reverse_Passage(x, P, Q, n);
 }
--- Numerical_Methods/lab1/main.cpp
+++ Numerical_Methods/lab1/main.cpp
@@ -1,8 +1,9 @@
 #include "lab1-1.hpp"
+#include "lab1-2.hpp"
 
 using namespace std;
 
-bool Check_The_Result (vector <vector <double>> &A,
+void Check_The_Result (vector <vector <double>> &A,
                        vector <double> &x,
                        vector <double> &b){ 
     double my_b;
@@ -11,10 +12,13 @@ bool Check_The_Result (vector <vector <double>> &A,
         for(int j = 0; j < x.size(); j++){
             my_b += A[i][j]*x[j];
         }
-        if(my_b != b[i])
-            return false;
+        if(my_b != b[i]){
+            cout << "неправильно решил" << endl;
+            return;
+        }
     }
-    return true;
+    for(double& xi: x)
+            cout << xi << endl;
 }
 
 void Read_SLAU (vector <vector<double>> &A,
@@ -34,10 +38,9 @@ int main(){
     vector <double> b(n, 0), x(n, 0);
     Read_SLAU(A, b, n);
     Solve_With_LU(A, b, x, n);
-    if(Check_The_Result(A, x, b))
-        for(double& xi: x)
-            cout << xi << endl;
-    else 
-        cout << "не правильно решил" << endl;
+    Check_The_Result(A, x, b);
+    cout << endl;
+    Solve_With_Run_Through(A, b, x, n);
+    Check_The_Result(A, x, b);
     return 0;
 }
